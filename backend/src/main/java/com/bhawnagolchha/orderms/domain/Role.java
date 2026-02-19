1cb6dcef4aceb2ad137af4ac621fc3720f105c6e
bhawna442 <bhawnagolchha03@gmail.com>
2026-02-19 21:15:14 +0530

    Added User Entity

---
+++ backend/src/main/java/com/bhawnagolchha/orderms/domain/Role.java
@@ -0,0 +1,6 @@
+package com.bhawnagolchha.orderms.domain;
+
+public enum Role {
+    ADMIN,
+    USER;
+}
---
+++ backend/src/main/java/com/bhawnagolchha/orderms/domain/User.java
@@ -0,0 +1,31 @@
+package com.bhawnagolchha.orderms.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @OneToMany(mappedBy = 'user', cascade = CascadeType.ALL)
+    private List<Order> orders;
+}
