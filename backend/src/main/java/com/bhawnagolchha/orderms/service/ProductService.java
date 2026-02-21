5d5680faf8a6acf24b52aa72f57eba0a56645227
bhawna442 <bhawnagolchha03@gmail.com>
2026-02-21 16:53:06 +0530

    Product Service added

---
+++ backend/src/main/java/com/bhawnagolchha/orderms/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.bhawnagolchha.orderms.service;
+
+import com.bhawnagolchha.orderms.domain.Product;
+import com.bhawnagolchha.orderms.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts(){
+        return productRepository.findAll();
+    }
+
+    public Product createProduct(Product product){
+        return productRepository.save(product);
+    }
+
+    public Product getProduct(Long id){
+        return productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found"));
+    }
+}
