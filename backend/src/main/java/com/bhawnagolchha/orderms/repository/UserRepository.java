95d1b728e38aab4b386a115d1e8dccd47d4e7236
bhawna442 <bhawnagolchha03@gmail.com>
2026-02-21 16:09:41 +0530

    Product repository added

--- backend/src/main/java/com/bhawnagolchha/orderms/repository/UserRepository.java
+++ backend/src/main/java/com/bhawnagolchha/orderms/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.bhawnagolchha.orderms.repository;
 
-public class UserRepository {
+import com.bhawnagolchha.orderms.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
