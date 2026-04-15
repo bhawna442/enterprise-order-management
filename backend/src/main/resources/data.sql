3f275afe6dab5597dd5daf7f2e3762a058765aee
bhawna442 <bhawnagolchha03@gmail.com>
2026-04-15 23:58:52 +0530

    DB changed to H2

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO users (id, name) VALUES (1, 'Test User');
-INSERT INTO product (id, name, price) VALUES (1, 'Test Product', 100);
+INSERT INTO products (id, name, price) VALUES (1, 'Test Product', 100);
